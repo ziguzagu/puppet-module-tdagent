dfee90271ee6c013917d85a621d0c1a89b1afcbf
ziguzagu <ziguzagu@gmail.com>
2013-04-09 23:12:25 +0900

    install td-agent rpm from treasure-data.com with adding yum repo.

---
+++ manifests/install.pp
@@ -0,0 +1,20 @@
+class td-agent::install {
+  case $operatingsystem {
+    /^(CentOS|RedHat)$/: { include td-agent::install::yum }
+    default:             { notice('Not supported OS. Please install td-agent by hand.') }
+  }
+}
+
+class td-agent::install::yum {
+  yumrepo { 'treasuredata':
+    baseurl  => 'http://packages.treasure-data.com/redhat/$architecture',
+    descr    => 'TreasureData',
+    enabled  => 1,
+    gpgcheck => 0
+  }
+
+  package { 'td-agent':
+    ensure  => 'installed',
+    require => Yumrepo['treasuredata'],
+  }
+}
