01694155c0042b9f7e2364c02b960fc5f89fbdef
ziguzagu <ziguzagu@gmail.com>
2013-07-29 23:00:47 +0900

    Use refactored class structure.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,32 +1,5 @@
 class td-agent {
-  package { 'td-agent':
-    ensure  => 'installed',
-  }
-
-  service { 'td-agent':
-    ensure     => 'running',
-    enable     => true,
-    hasrestart => true,
-    restart    => '/etc/init.d/td-agent reload',
-    require    => Package['td-agent'],
-  }
-
-  file { '/etc/td-agent/conf.d':
-    ensure  => 'directory',
-    owner   => 'root',
-    group   => 'root',
-    mode    => 0755,
-    recurse => false,
-    require => Package['td-agent'],
-  }
-
-  file { '/etc/td-agent/td-agent.conf':
-    ensure  => 'file',
-    owner   => 'root',
-    group   => 'root',
-    mode    => 0644,
-    source  => 'puppet:///modules/td-agent/td-agent.conf',
-    require => Package['td-agent'],
-    notify  => Service['td-agent'],
-  }
+  include td-agent::install
+  include td-agent::config
+  include td-agent::service
 }
