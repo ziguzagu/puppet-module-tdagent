e81f798eb9e68b02eccbcbe63abcf3640c4b3480
(cdep)illabout <cdep.illabout@gmail.com>
2014-04-21 15:41:57 +0900

    Added gpgkey for treasuredata repo.
    The treasuredata repo was not using a gpgkey.  I added this key and make
    it be installed before the treasuredata yum repo is added.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,24 @@
-class td_agent::install {
+class td_agent::install (
+  $gpgcheck = '1'
+) {
   yumrepo { 'treasuredata':
     baseurl  => 'http://packages.treasure-data.com/redhat/$basearch/',
     descr    => 'TreasureData',
     enabled  => 1,
-    gpgcheck => 0
+    gpgcheck => $gpgcheck,
+  }
+
+  file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-td-agent":
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/td_agent/RPM-GPG-KEY-td-agent',
+  }
+
+  td_agent::rpm_gpg_key{ "td-agent":
+    path    => '/etc/pki/rpm-gpg/RPM-GPG-KEY-td-agent',
+    before  => Yumrepo['treasuredata'],
   }
 
   package { 'td-agent':
